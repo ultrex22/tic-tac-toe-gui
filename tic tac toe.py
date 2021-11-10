d07fb823e0c2d59b840c231a55c10455c0e74356
Ronald <rontbrilhante@Gmail.com>
2021-11-09 21:22:13 -0500

    IMPLIMENTED CHECKING FOR WIN

--- tic tac toe.py
+++ tic tac toe.py
@@ -28,6 +28,11 @@ class Window(QMainWindow):
         self._createTitle()
         self._createGrid()
         self._createNames()
+        self.current_player = 'x'
+        self.grid_squares_filled = {
+            '1': 'F', '2': 'F', '3': 'F',
+            '4': 'F', '5': 'F', '6': 'F',
+            '7': 'F', '8': 'F', '9': 'F', }
         # center app/ widget
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
@@ -37,68 +42,107 @@ class Window(QMainWindow):
     def _createTitle(self):
         """Create the display."""
         # Create the display widget
-        self.display = QLabel('Welcome to Tic Tac Toe')
-        self.display.setStyleSheet('QLabel {color: #0000ee; font-size: 20px;}')
-        # Set some display's properties
-        self.display.setFixedHeight(35)
-        self.display.setAlignment(Qt.AlignCenter)
+        self.title = QLabel('Welcome to Tic Tac Toe')
+        self.title.setStyleSheet('QLabel {color: #0000ee; font-size: 20px;}')
+        # Set some title's properties
+        self.title.setFixedHeight(35)
+        self.title.setAlignment(Qt.AlignCenter)
         # Add the display to the general layout
-        self.generalLayout.addWidget(self.display)
+        self.generalLayout.addWidget(self.title)
 
     def _createGrid(self):
-        """Create the ."""
-        self.grid = {}
+        """Create the grid"""
         grid_Layout = QGridLayout()
-        # Button | position on the QGridLayout
+        # Button number | position on the QGridLayout
         self.grid = {'1': (0, 0), '2': (0, 1), '3': (0, 2),
                      '4': (1, 0), '5': (1, 1), '6': (1, 2),
                      '7': (2, 0), '8': (2, 1), '9': (2, 2)
                      }
         # Create the squares and add them to the grid layout
-        for square_txt, pos in self.grid.items():
+        for square_num, pos in self.grid.items():
             icon = QPixmap('blank.png')
-            self.grid[square_txt] = QPushButton(QIcon(icon), "")
-            self.grid[square_txt].setFixedSize(80, 80)
-            self.grid[square_txt].setIconSize(QSize(70, 70))
-            grid_Layout.addWidget(self.grid[square_txt], pos[0], pos[1])
+            self.grid[square_num] = QPushButton(QIcon(icon), square_num)
+            self.grid[square_num].setFixedSize(80, 80)
+            self.grid[square_num].setIconSize(QSize(70, 70))
+            grid_Layout.addWidget(self.grid[square_num], pos[0], pos[1])
         # Add grid_Layout to the general layout
         self.generalLayout.addLayout(grid_Layout)
-        self.grid['1'].clicked.connect(self.player_o_turn)
-        self.grid['1'] = QPushButton(QIcon(QPixmap('x_icon.png')), "")
+        self.grid['1'].clicked.connect(
+            lambda: self.update_grid(self.grid['1']))
+        self.grid['2'].clicked.connect(
+            lambda: self.update_grid(self.grid['2']))
+        self.grid['3'].clicked.connect(
+            lambda: self.update_grid(self.grid['3']))
+        self.grid['4'].clicked.connect(
+            lambda: self.update_grid(self.grid['4']))
+        self.grid['5'].clicked.connect(
+            lambda: self.update_grid(self.grid['5']))
+        self.grid['6'].clicked.connect(
+            lambda: self.update_grid(self.grid['6']))
+        self.grid['7'].clicked.connect(
+            lambda: self.update_grid(self.grid['7']))
+        self.grid['8'].clicked.connect(
+            lambda: self.update_grid(self.grid['8']))
+        self.grid['9'].clicked.connect(
+            lambda: self.update_grid(self.grid['9']))
 
     def _createNames(self):
-        """Create the player plaque."""
-        # Create the display widget
-        self.display = QLineEdit('Player X Turn')
-        self.display.setReadOnly(True)
+        """Create the player plaques."""
+        # Create the names widget
+        self.player_x = QLineEdit('Player X Turn')
+        self.player_x.setReadOnly(True)
         # Set some display's properties
-        self.display.setFixedHeight(30)
-        self.display.setAlignment(Qt.AlignCenter)
-        self.display.setStyleSheet(
+        self.player_x.setFixedHeight(30)
+        self.player_x.setAlignment(Qt.AlignCenter)
+        self.player_x.setStyleSheet(
             'QLineEdit {background: #11cc00; color: white;}')
         # Add the display to the general layout
-        self.generalLayout.addWidget(self.display)
+        self.generalLayout.addWidget(self.player_x)
 
-        self.display2 = QLineEdit('Player O Turn')
-        self.display2.setReadOnly(True)
+        self.player_o = QLineEdit('Player O Turn')
+        self.player_o.setReadOnly(True)
         # Set some display's properties
-        self.display2.setFixedHeight(30)
-        self.display2.setAlignment(Qt.AlignCenter)
+        self.player_o.setFixedHeight(30)
+        self.player_o.setAlignment(Qt.AlignCenter)
         # Add the display to the general layout
-        self.generalLayout.addWidget(self.display2)
+        self.generalLayout.addWidget(self.player_o)
 
-    def player_o_turn(self):
-        self.display2.setStyleSheet(
-            'QLineEdit {background: #11cc00; color: white;}')
-        self.display.setStyleSheet(
-            'QLineEdit {background: white; color: black;}')
-        self.grid['1']
+    def update_grid(self, grid_item):
+        print(self.grid_squares_filled[grid_item.text()])
+        if self.grid_squares_filled[grid_item.text()] == 'F':
+            self.grid_squares_filled[grid_item.text()] = self.current_player
+            self.check_for_winner()
+            if self.current_player == 'x':
+                self.player_o.setStyleSheet(
+                    'QLineEdit {background: #11cc00; color: white;}')
+                self.player_x.setStyleSheet(
+                    'QLineEdit {background: white; color: black;}')
+                grid_item.setIcon(QIcon(QPixmap('x_icon.png')))
+                self.current_player = 'o'
+            elif self.current_player == 'o':
+                self.player_x.setStyleSheet(
+                    'QLineEdit {background: #11cc00; color: white;}')
+                self.player_o.setStyleSheet(
+                    'QLineEdit {background: white; color: black;}')
+                grid_item.setIcon(QIcon(QPixmap('o_icon.png')))
+                self.current_player = 'x'
+        else:
+            pass
 
-    def player_x_turn(self):
-        pass
-
-    def is_winner(self):
-        pass
+    def check_for_winner(self):
+        print('checking for winner or tie...')
+        if (self.grid_squares_filled['1'] == self.grid_squares_filled['2'] == self.grid_squares_filled['3'] and self.grid_squares_filled['1'] != 'F' or
+            self.grid_squares_filled['4'] == self.grid_squares_filled['5'] == self.grid_squares_filled['6'] and self.grid_squares_filled['4'] != 'F' or
+            self.grid_squares_filled['7'] == self.grid_squares_filled['8'] == self.grid_squares_filled['9'] and self.grid_squares_filled['7'] != 'F' or
+            self.grid_squares_filled['7'] == self.grid_squares_filled['4'] == self.grid_squares_filled['1'] and self.grid_squares_filled['7'] != 'F' or
+            self.grid_squares_filled['8'] == self.grid_squares_filled['5'] == self.grid_squares_filled['2'] and self.grid_squares_filled['8'] != 'F' or
+            self.grid_squares_filled['9'] == self.grid_squares_filled['6'] == self.grid_squares_filled['3'] and self.grid_squares_filled['9'] != 'F' or
+            self.grid_squares_filled['7'] == self.grid_squares_filled['5'] == self.grid_squares_filled['3'] and self.grid_squares_filled['7'] != 'F' or
+                self.grid_squares_filled['9'] == self.grid_squares_filled['5'] == self.grid_squares_filled['1'] and self.grid_squares_filled['9'] != 'F'):
+            print(f'You win {self.current_player.upper()}!')
+            self.title.setText(f'You win {self.current_player.upper()}!')
+        elif len(self.grid_squares_filled) == 12:
+            self.title.setText('TIE !!!')
 
 
 def main():
